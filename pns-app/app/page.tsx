3938a4b555c667e4b3d8e98ac335e8cf2356b8e6
miguelrfernandes <miguelroquefernandes@gmail.com>
2025-05-09 22:11:40 +0100

    Enhance main page functionality by adding name availability check, wallet connection handling, and improved UI for name registration process.

--- pns-app/app/page.tsx
+++ pns-app/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { useReadContract,  } from 'wagmi';
+import { useAccount, useReadContract, useWriteContract,  } from 'wagmi';
 import { abi } from './abi';
 import { useState, useEffect } from "react";
 
@@ -10,6 +10,8 @@ const CONTRACT_ADDRESS = '0x6938A48508DD26027aBF887A73255f1fcD890953';
 export default function Home() {
   const [name, setName] = useState('');
   const [address, setAddress] = useState('');
+  const [nameAvailable, setNameAvailable] = useState(true);
+  const {writeContract} = useWriteContract();
   const { data, refetch } = useReadContract({
     address: CONTRACT_ADDRESS,
     abi,
@@ -20,25 +22,45 @@ export default function Home() {
   useEffect(() => {
     if (data) {
       setAddress(data.toString());
+      if (data.toString() === '0x0000000000000000000000000000000000000000') {
+        setNameAvailable(true);
+      } else {
+        setNameAvailable(false);
+      }
     }
   }, [data]);
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+    refetch();
+  };
+
+  const handleRegister = () => {
+    writeContract({
+      address: CONTRACT_ADDRESS,
+      abi,
+      functionName: 'register',
+      args: [name],
+    });
+  };
+
+  const {isConnected} = useAccount();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex items-center gap-4">
+        <div className="flex flex-col items-center gap-4">
           <ConnectButton />
           <h1 className="text-2xl font-bold">Human-readable names for Polkadot</h1>
           <h2 className="text-sm">Link names to your wallet address.</h2>
           <h2 className="text-sm">Simple, fast, and decentralized.</h2>
-        </div>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <div>
-            <p>Name:</p>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            <button onClick={() => refetch()}>Get Address</button>
-            <p>Address: {address}</p>
-          </div>
+          <input className="border-2 border-gray-300 rounded-md p-2 bg-gray-100 text-black"
+          type="text" value={name} onChange={handleNameChange} />
+          <button className="bg-blue-500 text-white p-2 rounded-md"
+            disabled={!nameAvailable} onClick={handleRegister}
+            >
+                {!isConnected ? 'Connect your wallet' : nameAvailable ? 'Get your name now 🚀' : 'Name already taken'}</button>
+          {!nameAvailable && <p>Address: {address}</p>}
         </div>
       </main>
     </div>
