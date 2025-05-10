acf490b077c93969bdf2c4ec4e172f9061846250
helenaboing <helenaboingg+github@gmail.com>
2025-05-10 14:08:35 +0100

    Refactor Home component header for improved layout and update GitHub link. Enhance Providers component with dark theme configuration for RainbowKit.

--- pns-app/app/page.tsx
+++ pns-app/app/page.tsx
@@ -68,11 +68,11 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="flex items-center justify-between w-full">
-        <Image src="/logo.svg" alt="logo" width={100} height={100} />
-        <div className="flex items-center gap-2">
+      <header className="flex items-center justify-between w-full p-[30px]">
+        <Image src="/logo.svg" alt="logo" width={200} height={70} />
+        <div className="flex items-center gap-[20px]">
           <ConnectButton />
-          <Link href="https://github.com/polkadot-labs/pns">
+          <Link href="https://github.com/mokita-j/pns">
             <SiGithub />
           </Link>
         </div>
