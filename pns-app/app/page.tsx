19a823c0b6750b8964ccfaeee0953ebdc1c3648d
helenaboing <helenaboingg+github@gmail.com>
2025-05-10 13:49:06 +0100

    Update header layout in Home component to use SVG logo and improve alignment with GitHub link

--- pns-app/app/page.tsx
+++ pns-app/app/page.tsx
@@ -68,9 +68,9 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header>
-        <Image src="/logo.png" alt="logo" width={100} height={100} />
-        <div>
+      <header className="flex items-center justify-between w-full">
+        <Image src="/logo.svg" alt="logo" width={100} height={100} />
+        <div className="flex items-center gap-2">
           <ConnectButton />
           <Link href="https://github.com/polkadot-labs/pns">
             <SiGithub />
