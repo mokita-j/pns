d2c6e65ba8f695061f1c4aea566934ad6754a937
Monica Jin <monicachenjin@gmail.com>
2025-05-11 03:51:23 +0100

    refactor: registry ignition

--- pns-v2/ignition/modules/PNSRegistry.js
+++ pns-v2/ignition/modules/PNSRegistry.js
@@ -4,11 +4,10 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 const PNSRegistryModule = buildModule("PNSRegistryModule", (m) => {
-  const initialSupply = m.getParameter("initialSupply", 1000000n * 10n ** 18n);
 
-  const token = m.contract("PNSRegistry", []);
+  const registry = m.contract("PNSRegistry", []);
 
-  return { token };
+  return { registry };
 });
 
 module.exports = PNSRegistryModule;
